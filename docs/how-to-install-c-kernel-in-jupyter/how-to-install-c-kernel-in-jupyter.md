7d94dcba2c8c8ae17c85a9ed22981a499bca43f0
Clayton Seitz <cwseitz2@gmail.com>
2021-01-14 22:37:12 -0500

    update docs

--- docs/how-to-install-c-kernel-in-jupyter/how-to-install-c-kernel-in-jupyter.md
+++
@@ -1,7 +0,0 @@
-```python
-conda activate XXX
-pip install jupyter-c-kernel
-sudo mkdir /usr/local/share/jupyter
-chmod -R 777 /usr/local/share/jupyter
-install_c_kernel
-```
--- util/_util.py
+++ util/_util.py
@@ -5,7 +5,9 @@ def excel_to_html(file):
     posts = pd.read_excel(file)
     posts['link'] = '<a href="' + posts['filename'] + '">' + posts['title'] + '</a>'
     del posts['filename']; del posts['title']
-    posts = posts.reindex(sorted(posts.columns), axis=1)
+     
+    #posts = posts.reindex(sorted(posts.columns), axis=1)
+    posts = posts.reindex(['link', 'subject', 'type'], axis=1)
     posts = posts.fillna('')
     cols = posts.columns
 
@@ -13,6 +15,7 @@ def excel_to_html(file):
     html = html.replace('border="1"','border="0" id="myTable"')
     html = html.replace('<th>link</th>', '<th onclick="sortTable(0)">Title</th>')
     html = html.replace('<th>subject</th>', '<th onclick="sortTable(1)">Subject</th>') 
-    html = html.replace('<th>group</th>', '<th onclick="sortTable(2)">Group</th>')
+    #html = html.replace('<th>group</th>', '<th onclick="sortTable(2)">Group</th>')
+    html = html.replace('<th>type</th>', '<th onclick="sortTable(2)">Type</th>')
     
     return html
