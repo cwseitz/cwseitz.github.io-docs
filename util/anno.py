53d32fc73dd40b60053021456c80d47f902801b3
Clayton Seitz <cwseitz2@gmail.com>
2021-04-16 20:42:13 -0400

    new photos

--- util/anno.py
+++ util/anno.py
@@ -4,6 +4,11 @@ from PIL import ImageFont
 from PIL import ImageDraw 
 from glob import glob
 
+from skimage.io import imread, imsave
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def get_exif(file):
 	
 	f = open(file, 'rb')
@@ -19,6 +24,16 @@ def get_exif(file):
 
 	return str
 
+def add_matte(im, pad=(300,300), value=255):
+
+	pad_h, pad_w  = pad
+	r  = np.pad(im[:,:,0], ((pad_h,pad_w),(pad_h,pad_w)), mode='constant', constant_values=((value, value),(value,value)))	
+	g  = np.pad(im[:,:,1], ((pad_h,pad_w),(pad_h,pad_w)), mode='constant', constant_values=((value, value),(value,value)))	
+	b  = np.pad(im[:,:,2], ((pad_h,pad_w),(pad_h,pad_w)), mode='constant', constant_values=((value, value),(value,value)))	
+	im = np.dstack((r,g,b))
+	return im	
+
+
 def anno_exif(file, str, box_size=(600,400), out_file=None):
 
 	"""Annotates EXIF parameters onto image"""
@@ -26,20 +41,24 @@ def anno_exif(file, str, box_size=(600,400), out_file=None):
 	if not out_file:
 		out_file=file
 
-	img = Image.open(file)
-	width, height = img.size
-	rect_width, rect_height = box_size
+	#img = Image.open(file)
+	#width, height = img.size
+	#rect_width, rect_height = box_size
 
-	font = ImageFont.truetype('assets/arial.ttf', 50)
-	draw = ImageDraw.Draw(img)
-	draw.rectangle((width-rect_width, height-rect_height, width, height), fill='black')
-	draw.text((width-rect_width,height-rect_height),str,(255,255,255), font=font)
-
-	img.save(out_file)
-
+	#font = ImageFont.truetype('assets/arial.ttf', 50)
+	#draw = ImageDraw.Draw(img)
+	#draw.rectangle((width-rect_width, height-rect_height, width, height), fill='black')
+	#draw.text((width-rect_width,height-rect_height),str,(255,255,255), font=font)
+	#img.save(out_file)
 
 str = 'photos/*.jpg'
 files = glob(str)
 for file in files:
+
+	im = imread(file)
+	im = add_matte(im, pad=(10, 10), value=0)
+	im = add_matte(im, pad=(300,300), value=255)
+	im = add_matte(im, pad=(10, 10), value=0)
+
 	exif = get_exif(file)
-	anno_exif(file, exif)
+	#anno_exif(file, exif)
