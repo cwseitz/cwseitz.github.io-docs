267d5107f5fdd844b3da52d01dabcbffd06069b4
cwseitz <cwseitz2@gmail.com>
2021-09-09 16:13:03 -0500

    update docs

--- docs/iplm/micro-rna/micro-rna/analysis.py
+++ docs/iplm/micro-rna/micro-rna/analysis.py
@@ -61,14 +61,24 @@ for file in files:
     this_df = this_df.loc[:, ~this_df.columns.str.contains('^Unnamed')]
     df = pd.concat([df, this_df])
 
+"""
+Print data summary
+"""
+
+df_t1d = df.loc[df['group'] == 'T1D']
+df_ctrl = df.loc[df['group'] == 'No diabetes']
+
+u = df_ctrl[['file', 'label']].drop_duplicates()
+v = df_t1d[['file', 'label']].drop_duplicates()
+print('Control cells: ', u.shape[0])
+print('T1D cells: ', v.shape[0])
 
 """
 Boxplot Sanity Check
 """
 
 fig, ax = plt.subplots()
-df_t1d = df.loc[df['group'] == 'T1D']
-df_ctrl = df.loc[df['group'] == 'No diabetes']
+
 
 counts_t1d = df_t1d.groupby(['label', 'file', 'group']).size().reset_index(name='count')
 counts_ctrl = df_ctrl.groupby(['label', 'file', 'group']).size().reset_index(name='count')
@@ -78,54 +88,54 @@ ax.set_ylabel('miRNA-146 Count')
 plt.show()
 
 
-"""
-PCA
-"""
-
-#preprocessing
-def preprocess(df):
-
-    x = df.values
-    min_max_scaler = preprocessing.MinMaxScaler()
-    x_scaled = min_max_scaler.fit_transform(x)
-    df = pd.DataFrame(x_scaled)
-
-    return df
-k = list(new_feature_names.values())
-
-df_t1d = preprocess(df_t1d[k])
-df_ctrl = preprocess(df_ctrl[k])
-df_t1d.columns = k
-df_ctrl.columns = k
-
-t1d = df_t1d.to_numpy()
-t1d_pca = PCA(n_components=2)
-t1d_pca.fit(t1d)
-t1d = t1d_pca.transform(t1d)
-
-ctrl = df_ctrl.to_numpy()
-ctrl_pca = PCA(n_components=2)
-ctrl_pca.fit(ctrl)
-ctrl = ctrl_pca.transform(ctrl)
-
-plt.scatter(t1d[:,0], t1d[:,1], color='red')
-plt.scatter(ctrl[:,0], ctrl[:,1], color='blue')
-plt.show()
-
-fig, ax = plt.subplots()
-ax.plot(t1d_pca.components_[0], color='red', label='T1D PC1')
-ax.plot(ctrl_pca.components_[0], color='blue', label='CTRL PC1')
-ax.plot(t1d_pca.components_[1], linestyle='--', color='red', label='T1D PC2')
-ax.plot(ctrl_pca.components_[1], linestyle='--', color='blue', label='CTRL PC2')
-ax.xaxis.set_ticks(np.arange(0, len(k), 1))
-ax.set_xticklabels(k, rotation=90)
-plt.tight_layout()
-plt.legend()
-plt.show()
-
-
-fig, ax = plt.subplots()
-ax.hist(df_t1d['mean_dist'], color='red', label='T1D')
-ax.hist(df_ctrl['mean_dist'], color='blue', label='CTRL')
-plt.legend()
-plt.show()
+# """
+# PCA
+# """
+#
+# #preprocessing
+# def preprocess(df):
+#
+#     x = df.values
+#     min_max_scaler = preprocessing.MinMaxScaler()
+#     x_scaled = min_max_scaler.fit_transform(x)
+#     df = pd.DataFrame(x_scaled)
+#
+#     return df
+# k = list(new_feature_names.values())
+#
+# df_t1d = preprocess(df_t1d[k])
+# df_ctrl = preprocess(df_ctrl[k])
+# df_t1d.columns = k
+# df_ctrl.columns = k
+#
+# t1d = df_t1d.to_numpy()
+# t1d_pca = PCA(n_components=2)
+# t1d_pca.fit(t1d)
+# t1d = t1d_pca.transform(t1d)
+#
+# ctrl = df_ctrl.to_numpy()
+# ctrl_pca = PCA(n_components=2)
+# ctrl_pca.fit(ctrl)
+# ctrl = ctrl_pca.transform(ctrl)
+#
+# plt.scatter(t1d[:,0], t1d[:,1], color='red')
+# plt.scatter(ctrl[:,0], ctrl[:,1], color='blue')
+# plt.show()
+#
+# fig, ax = plt.subplots()
+# ax.plot(t1d_pca.components_[0], color='red', label='T1D PC1')
+# ax.plot(ctrl_pca.components_[0], color='blue', label='CTRL PC1')
+# ax.plot(t1d_pca.components_[1], linestyle='--', color='red', label='T1D PC2')
+# ax.plot(ctrl_pca.components_[1], linestyle='--', color='blue', label='CTRL PC2')
+# ax.xaxis.set_ticks(np.arange(0, len(k), 1))
+# ax.set_xticklabels(k, rotation=90)
+# plt.tight_layout()
+# plt.legend()
+# plt.show()
+#
+#
+# fig, ax = plt.subplots()
+# ax.hist(df_t1d['mean_dist'], color='red', label='T1D')
+# ax.hist(df_ctrl['mean_dist'], color='blue', label='CTRL')
+# plt.legend()
+# plt.show()
