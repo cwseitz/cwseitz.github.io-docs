885f0cf721a01be76084dccf8b8e687b94da17a3
cwseitz <cwseitz2@gmail.com>
2022-10-27 13:53:12 -0400

    update

--- docs/qm/p1/p1/p1.py
+++ docs/qm/p1/p1/p1.py
@@ -29,27 +29,34 @@ vals = vals[idx]
 ###########################################
 
 fig, ax = plt.subplots(1,2)
-ax[0].imshow(H,cmap='coolwarm')
-ax[1].imshow(H[:10,:10],cmap='coolwarm')
+x = ax[0].imshow(H,cmap='coolwarm')
+y = ax[1].imshow(H[:10,:10],cmap='coolwarm')
+plt.colorbar(x,ax=ax[0])
+plt.colorbar(y,ax=ax[1])
 plt.show()
 
 ###########################################
 # Show eigenvectors form an orthonormal set
 ###########################################
 
-fig, ax = plt.subplots()
-ax.imshow(vecs.T @ vecs, cmap='coolwarm')
+fig, ax = plt.subplots(1,3)
+ax[0].imshow(vecs.T, cmap='coolwarm')
+ax[0].set_title(r'$T$')
+ax[1].imshow(vecs, cmap='coolwarm')
+ax[1].set_title(r'$T^{T}$')
+ax[2].imshow(vecs.T @ vecs, cmap='coolwarm')
+ax[2].set_title(r'$T^{T}T$')
 plt.show()
 
 ###########################################
 # Plot the eigenvalues as a function of n
 ###########################################
 
-fig, ax = plt.subplots()
-ax.plot(vals, color='blue')
-ax.set_xlabel('n')
-ax.set_ylabel(r'$\epsilon_{0,n}$',fontsize=14)
-plt.show()
+fig, ax = plt.subplots(1,2)
+ax[0].plot(vals, color='blue')
+ax[0].set_xlabel('n')
+ax[0].set_ylabel(r'$\epsilon_{0,n}$',fontsize=14)
+
 
 ###########################################
 # Plot a few representative prob dists
@@ -59,16 +66,15 @@ pdf1 = vecs[:,0]**2
 pdf2 = vecs[:,10]**2
 pdf3 = vecs[:,50]**2
 
-fig, ax = plt.subplots()
-ax1 = ax.twinx()
+ax1 = ax[1].twinx()
 ax1.plot(V,color='black',label='V(x)')
-ax.plot(pdf1, color='red',label='m=0')
-ax.plot(pdf2, color='blue',label='m=10')
-ax.plot(pdf3, color='purple',label='m=50')
-ax.set_xlabel('x')
-ax.set_ylabel(r'$|\phi_{m,n}|^{2}$',fontsize=14)
+ax[1].plot(pdf1, color='red',label='m=0')
+ax[1].plot(pdf2, color='blue',label='m=10')
+ax[1].plot(pdf3, color='purple',label='m=50')
+ax[1].set_xlabel('x')
+ax[1].set_ylabel(r'$|\phi_{m,n}|^{2}$',fontsize=14)
 ax1.set_ylabel('V(x)')
-ax.legend()
+ax[1].legend()
 plt.show()
 
 ###########################################
