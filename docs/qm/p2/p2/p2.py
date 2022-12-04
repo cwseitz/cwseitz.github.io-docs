056683583a23fd8ab513b0294980eabd8d8230fc
cwseitz <cwseitz2@gmail.com>
2022-12-04 09:59:02 -0500

    update

--- docs/qm/p2/p2/p2.py
+++ docs/qm/p2/p2/p2.py
@@ -28,25 +28,41 @@ class FDTDSolver:
 Nx = 100
 Nt = 100
 
+N = Nx
+Vl = 2
+Vr = 3
+t = 1
+V = np.zeros((N,))
+V[:30] = Vl; V[70:] = Vr
+H = np.zeros((N,N))
+H += np.diag(2*t + V,k=0) #main diagonal
+H += np.diag(-t*np.ones((N-1,)),k=1) #upper diagonal
+H += np.diag(-t*np.ones((N-1,)),k=-1) #lower diagonal
+
 ###########################################
-# Find eigenvectors and eigenvalues of H0
+# Find eigenvectors and eigenvalues of H
 ###########################################
 
-V = np.zeros((Nx,)) #Potential
-H0 = np.zeros((Nx,Nx)) #Hamiltonian matrix H0
-H0 += np.diag(2 + V,k=0) #main diagonal
-H0 += np.diag(-1*np.ones((Nx-1,)),k=1) #upper diagonal
-H0 += np.diag(-1*np.ones((Nx-1,)),k=-1) #lower diagonal
-vals, vecs = LA.eig(H0)
+vals, vecs = LA.eig(H)
 idx = np.argsort(vals)
-vecs = vecs[idx]
+vecs = vecs[:,idx]
 vals = vals[idx]
 
 ###########################################
 # Simulate the ground state wavefunction
 ###########################################
 
-psi_r0 = vecs[0] #ground state
+psi_r0 = vecs[:,5] #ground state
 psi_i0 = np.zeros((Nx,1))
 solver = FDTDSolver(Nx,Nt,V,psi_r0,psi_i0)
 solver.forward()
+
+fig, ax = plt.subplots()
+ax.plot(solver.psi_r[0,:],color='red')
+ax.plot(solver.psi_i[0,:],color='blue')
+plt.show()
+
+fig, ax = plt.subplots()
+ax.plot(solver.psi_r[2,:],color='red')
+ax.plot(solver.psi_i[2,:],color='blue')
+plt.show()
