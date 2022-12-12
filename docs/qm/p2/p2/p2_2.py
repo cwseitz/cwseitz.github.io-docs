b96d305cddbfd2500221b7aa3a65ca882be2cabb
cwseitz <cwseitz2@gmail.com>
2022-12-12 16:15:24 -0500

    update

--- docs/qm/p2/p2/p2_2.py
+++ docs/qm/p2/p2/p2_2.py
@@ -56,7 +56,7 @@ P_0n /= (vals1[1:]-E0)**2
 # Compute transition probability for n=1,2,3
 #############################################
 
-tau = np.arange(0,Nt*3)*dtau
+tau = np.arange(0,Nt)*dtau
 v_11 = Hp_e[0,0]
 v_12 = Hp_e[0,1]
 v_13 = Hp_e[0,2]
@@ -69,19 +69,16 @@ c_12_msq /= Z
 c_13_msq /= Z
 Ebar = c_11_msq*vals1[0] + c_12_msq*vals1[1] + c_13_msq*vals1[2]
 
-fig, ax = plt.subplots(2,2,figsize=(4,3))
-ax[0,0].plot(tau,c_11_msq,color='red',label=r'$|c_{11}|^{2}$')
-ax[0,1].plot(tau,c_12_msq,color='blue',label=r'$|c_{12}|^{2}$')
-ax[1,0].plot(tau,c_13_msq,color='purple',label=r'$|c_{13}|^{2}$')
-ax[1,1].plot(tau,Ebar,color='cyan')
-ax[0,0].set_xlabel(r'$\tau$')
-ax[0,1].set_xlabel(r'$\tau$')
-ax[1,0].set_xlabel(r'$\tau$')
-ax[1,1].set_xlabel(r'$\tau$')
-ax[0,0].set_ylabel(r'$P(1\rightarrow 1)$')
-ax[1,0].set_ylabel(r'$P(1\rightarrow 2)$')
-ax[0,1].set_ylabel(r'$P(1\rightarrow 3)$')
-ax[1,1].set_ylabel(r'$\langle H \rangle$')
+fig, ax = plt.subplots(1,3,figsize=(7,2))
+ax[0].plot(tau,c_11_msq,color='red',label=r'$|c_{11}|^{2}$')
+ax[1].plot(tau,c_12_msq,color='blue',label=r'$|c_{12}|^{2}$')
+ax[2].plot(tau,c_13_msq,color='purple',label=r'$|c_{13}|^{2}$')
+ax[0].set_xlabel(r'$\tau$')
+ax[1].set_xlabel(r'$\tau$')
+ax[2].set_xlabel(r'$\tau$')
+ax[0].set_ylabel(r'$P(1\rightarrow 1)$')
+ax[1].set_ylabel(r'$P(1\rightarrow 2)$')
+ax[2].set_ylabel(r'$P(1\rightarrow 3)$')
 plt.tight_layout()
 plt.show()
 
@@ -89,25 +86,36 @@ plt.show()
 # Simulate time evolution for a time dependent Hamiltonian
 ##################################################################
 
-# V = np.pad(V, ((1,1),(0,0)))
-# psi_r0 = -1*vecs1[:,0] #pure ground state
-# psi_i0 = np.zeros_like(psi_r0)
-# solver = FDTDSolver(Nx,Nt,V,psi_r0,psi_i0,dir,plot_iter_num=5000,plot=True,dt=dtau,H=H+Hp)
-# solver.forward()
+
+V = np.pad(V, ((1,1),(0,0)))
+psi_r0 = -1*vecs1[:,0] #pure ground state
+psi_i0 = np.zeros_like(psi_r0)
+solver = FDTDSolver(Nx,Nt,V,psi_r0,psi_i0,dir,plot_iter_num=5000,plot=True,dt=dtau,H=H+Hp)
+solver.forward()
+
 
 ##################################################################
-# Construction time evolution analytically
+# Construct time evolution analytically
 ##################################################################
 
 probt = np.zeros((Nx,Nt))
 probt[:,0] = vecs1[:,0]**2
+print(np.sum(probt[:,0]))
 for t in range(1,Nt):
-    psi = c_11_msq[t]*vecs1[:,0] + c_12_msq[t]*vecs1[:,1] + c_12_msq[t]*vecs1[:,2]
-    probt[:,t] = psi**2
-
+    psi = c_11_msq[t]*vecs1[:,0] + c_12_msq[t]*vecs1[:,1] + c_13_msq[t]*vecs1[:,2]
+    probt[:,t] = (psi**2)/np.sum(psi**2)
+p1 = probt[:,1]*np.arange(0,Nx)
 X_avg = probt.T * np.arange(0,Nx)
 X_avg = np.sum(X_avg,axis=1)
-plt.plot(X_avg)
+X_avg[0] = 50
+fig, ax = plt.subplots(1,2)
+ax[0].plot(tau,X_avg[0:],color='black')
+ax[0].set_xlabel(r'$\tau$')
+ax[0].set_ylabel(r'$\langle x/L\rangle$')
+ax[1].plot(tau,Ebar,color='black')
+ax[1].set_xlabel(r'$\tau$')
+ax[1].set_ylabel(r'$\langle H \rangle$')
+plt.tight_layout()
 plt.show()
 
 ##################################################################
