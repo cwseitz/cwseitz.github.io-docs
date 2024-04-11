c0d1b447d50d98c58594db8312f9353ee925ea52
cwseitz <cwseitz2@gmail.com>
2024-04-11 17:33:07 -0400

    update

--- docs/phd/nips/nips/errors.py
+++ docs/phd/nips/nips/errors.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 # Data
-N0_values = [200, 500, 1000]
+N0_values = [2,5,10]
 
 xerr = [0.8877236121056422,0.6374707169264318,0.5795656562318549]
 txerr = [1.1232689159068578,0.796919385395769,0.7301242902211198]
@@ -12,24 +12,24 @@ xerr = [27 * x for x in xerr]
 txerr = [27 * y for y in txerr]
 
 # Plotting
-fig, axs = plt.subplots(1, 2, figsize=(6, 3))
+fig, axs = plt.subplots(1, 2, figsize=(4, 2))
 
 #axs[0].set_xscale('log')
-axs[0].plot(N0_values, jaccard, marker='o', label='Diffusion')
-axs[0].plot(N0_values, tjaccard, marker='o', label='DeepSTORM')
-axs[0].set_xlabel(r'$N_0$')
+axs[0].plot(N0_values, jaccard, marker='o', color='black', label='Diffusion')
+axs[0].plot(N0_values, tjaccard, marker='o', color='gray', label='DeepSTORM')
+axs[0].set_xlabel('SNR')
 axs[0].set_ylabel('Jaccard Index')
-axs[0].legend()
+#axs[0].legend()
 
 # Localization Error
 #axs[1].set_xscale('log')
-axs[1].plot(N0_values, xerr, marker='o', label='Diffusion')
-axs[1].plot(N0_values, txerr, marker='o', label='DeepSTORM')
-axs[1].set_xlabel(r'$N_0$')
+axs[1].plot(N0_values, xerr, marker='o', color='black',label='Diffusion')
+axs[1].plot(N0_values, txerr, marker='o', color='gray', label='DeepSTORM')
+axs[1].set_xlabel('SNR')
 axs[1].set_ylabel('Localization Error (nm)')
 axs[1].legend()
 axs[1].set_xticks(N0_values)
-axs[1].set_xscale('log')
+#axs[1].set_xscale('log')
 plt.tight_layout()
 plt.show()
 
